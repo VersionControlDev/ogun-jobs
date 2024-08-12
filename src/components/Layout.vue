e834787667a5735f4c76bd6d68a2d5109e55c58d
iamta0 <sanusiheryourmih@gmail.com>
2024-08-12 15:52:18 +0100

    Feature: added landing page

---
+++ src/components/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Navbar />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import router from '../router';
+import Footer from './Footer.vue';
+import Navbar from './Navbar.vue';
+
+export default {
+    components: {
+        Navbar,
+        Footer
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <header>
+    <!-- Header Start -->
+    <div class="header-area header-transparrent">
+        <div class="headder-top header-sticky">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-3 col-md-2">
+                        <!-- Logo -->
+                        <div class="logo">
+                            <a href="index.html"><img style="width: 20%;" src="/assets/img/logo/EzpXe5xWYAAkpqo.jpg" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-9 col-md-9">
+                        <div class="menu-wrapper">
+                            <!-- Main-menu -->
+                            <div class="main-menu">
+                                <nav class="d-none d-lg-block">
+                                    <ul id="navigation">
+                                        <li><router-link to="/">Home</router-link></li>
+                                        <li><router-link to="/find-jobs">Find a Jobs </router-link></li>
+                                        <li><router-link to="/">About</router-link></li>
+                                        <li><a href="#">Page</a>
+                                            <ul class="submenu">
+                                                <li><a href="blog.html">Blog</a></li>
+                                                <li><a href="single-blog.html">Blog Details</a></li>
+                                                <li><a href="elements.html">Elements</a></li>
+                                                <li><a href="job_details.html">job Details</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="contact.html">Contact</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                            <!-- Header-btn -->
+                            <div class="header-btn d-none f-right d-lg-block">
+                                <a href="#" class="btn head-btn1">Register</a>
+                                <a href="#" class="btn head-btn2">Login</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Mobile Menu -->
+                    <div class="col-12">
+                        <div class="mobile_menu d-block d-lg-none"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Header End -->
+</header>
+</template>
+
+<script>
+export default {
+    name:'Navbar'
+}
+</script>
+
+<style>
+
+</style>
