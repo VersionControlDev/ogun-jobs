5a1bb36f76a2a9d2cfc9aebe0dc98c11c6beaebe
iamta0 <sanusiheryourmih@gmail.com>
2024-08-12 17:05:49 +0100

    landing-page

--- src/views/FindJobs.vue
+++ src/views/FindJobs.vue
@@ -54,7 +54,7 @@
                   </div>
                   <!-- Select job items start -->
                   <div class="select-job-items2">
-                    <select name="select">
+                    <select class="nice-select" name="select">
                       <option value="">All Category</option>
                       <option value="">Category 1</option>
                       <option value="">Category 2</option>
@@ -186,8 +186,55 @@
                     <div class="widgets_inner">
                       <div class="range_item">
                         <!-- <div id="slider-range"></div> -->
-                        <input type="text" class="js-range-slider" value="" />
-                        <div class="d-flex align-items-center">
+                        <span class="irs js-irs-0"
+                          ><span class="irs"
+                            ><span class="irs-line" tabindex="-1"
+                              ><span class="irs-line-left"></span
+                              ><span class="irs-line-mid"></span
+                              ><span class="irs-line-right"></span></span
+                            ><span class="irs-min" style="visibility: hidden"
+                              >tk. 0</span
+                            ><span class="irs-max" style="visibility: visible"
+                              >tk. 1.000</span
+                            ><span
+                              class="irs-from"
+                              style="visibility: visible; left: 0%"
+                              >tk. 0</span
+                            ><span
+                              class="irs-to"
+                              style="visibility: visible; left: 35.3448%"
+                              >tk. 500</span
+                            ><span
+                              class="irs-single"
+                              style="visibility: hidden; left: 4.45402%"
+                              >tk. 0 - tk. 500</span
+                            ></span
+                          ><span class="irs-grid"></span
+                          ><span
+                            class="irs-bar"
+                            style="left: 4.31034%; width: 45.6897%"
+                          ></span
+                          ><span
+                            class="irs-shadow shadow-from"
+                            style="display: none"
+                          ></span
+                          ><span
+                            class="irs-shadow shadow-to"
+                            style="display: none"
+                          ></span
+                          ><span class="irs-slider from" style="left: 0%"></span
+                          ><span
+                            class="irs-slider to type_last"
+                            style="left: 45.6897%"
+                          ></span
+                        ></span>
+                        <input
+                          type="text"
+                          class="js-range-slider irs-hidden-input"
+                          value=""
+                          readonly=""
+                        />
+                        <!-- <div class="d-flex align-items-center">
                           <div class="price_text">
                             <p>Price :</p>
                           </div>
@@ -206,6 +253,30 @@
                               class="js-input-to"
                               id="amount"
                               readonly
+                            />
+                          </div>
+                        </div> -->
+                        <div class="d-flex align-items-center">
+                          <div class="price_text">
+                            <p>Price :</p>
+                          </div>
+                          <div
+                            class="price_value d-flex justify-content-center"
+                          >
+                            <input
+                              type="text"
+                              class="js-input-from"
+                              id="amount"
+                              readonly=""
+                              value="0"
+                            />
+                            <span>to</span>
+                            <input
+                              type="text"
+                              class="js-input-to"
+                              id="amount"
+                              readonly=""
+                              value="500"
                             />
                           </div>
                         </div>
@@ -463,162 +534,6 @@
       </div>
       <!--Pagination End  -->
     </main>
-    <footer>
-      <!-- Footer Start-->
-      <div class="footer-area footer-bg footer-padding">
-        <div class="container">
-          <div class="row d-flex justify-content-between">
-            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6">
-              <div class="single-footer-caption mb-50">
-                <div class="single-footer-caption mb-30">
-                  <div class="footer-tittle">
-                    <h4>About Us</h4>
-                    <div class="footer-pera">
-                      <p>
-                        Heaven frucvitful doesn't cover lesser dvsays appear
-                        creeping seasons so behold.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
-              <div class="single-footer-caption mb-50">
-                <div class="footer-tittle">
-                  <h4>Contact Info</h4>
-                  <ul>
-                    <li>
-                      <p>Address :Your address goes here, your demo address.</p>
-                    </li>
-                    <li><a href="#">Phone : +8880 44338899</a></li>
-                    <li><a href="#">Email : info@colorlib.com</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
-              <div class="single-footer-caption mb-50">
-                <div class="footer-tittle">
-                  <h4>Important Link</h4>
-                  <ul>
-                    <li><a href="#"> View Project</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                    <li><a href="#">Testimonial</a></li>
-                    <li><a href="#">Proparties</a></li>
-                    <li><a href="#">Support</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
-              <div class="single-footer-caption mb-50">
-                <div class="footer-tittle">
-                  <h4>Newsletter</h4>
-                  <div class="footer-pera footer-pera2">
-                    <p>
-                      Heaven fruitful doesn't over lesser in days. Appear
-                      creeping.
-                    </p>
-                  </div>
-                  <!-- Form -->
-                  <div class="footer-form">
-                    <div id="mc_embed_signup">
-                      <form
-                        target="_blank"
-                        action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
-                        method="get"
-                        class="subscribe_form relative mail_part"
-                      >
-                        <input
-                          type="email"
-                          name="email"
-                          id="newsletter-form-email"
-                          placeholder="Email Address"
-                          class="placeholder hide-on-focus"
-                          onfocus="this.placeholder = ''"
-                          onblur="this.placeholder = ' Email Address '"
-                        />
-                        <div class="form-icon">
-                          <button
-                            type="submit"
-                            name="submit"
-                            id="newsletter-submit"
-                            class="email_icon newsletter-submit button-contactForm"
-                          >
-                            <img src="/assets/img/icon/form.png" alt="" />
-                          </button>
-                        </div>
-                        <div class="mt-10 info"></div>
-                      </form>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!--  -->
-          <div class="row footer-wejed justify-content-between">
-            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6">
-              <!-- logo -->
-              <div class="footer-logo mb-20">
-                <a href="index.html"
-                  ><img src="/assets/img/logo/logo2_footer.png" alt=""
-                /></a>
-              </div>
-            </div>
-            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
-              <div class="footer-tittle-bottom">
-                <span>5000+</span>
-                <p>Talented Hunter</p>
-              </div>
-            </div>
-            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
-              <div class="footer-tittle-bottom">
-                <span>451</span>
-                <p>Talented Hunter</p>
-              </div>
-            </div>
-            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
-              <!-- Footer Bottom Tittle -->
-              <div class="footer-tittle-bottom">
-                <span>568</span>
-                <p>Talented Hunter</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- footer-bottom area -->
-      <div class="footer-bottom-area footer-bg">
-        <div class="container">
-          <div class="footer-border">
-            <div class="row d-flex justify-content-between align-items-center">
-              <div class="col-xl-10 col-lg-10">
-                <div class="footer-copy-right">
-                  <p>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    Copyright &copy; All rights reserved | This template is made
-                    with <i class="fa fa-heart" aria-hidden="true"></i> by
-                    <a href="https://edutams.net" target="_blank">EduTAMS</a>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                  </p>
-                </div>
-              </div>
-              <div class="col-xl-2 col-lg-2">
-                <div class="footer-social f-right">
-                  <a href="#"><i class="fab fa-facebook-f"></i></a>
-                  <a href="#"><i class="fab fa-twitter"></i></a>
-                  <a href="#"><i class="fas fa-globe"></i></a>
-                  <a href="#"><i class="fab fa-behance"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- Footer End-->
-    </footer>
   </div>
 </template>
 
